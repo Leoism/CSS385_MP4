872197f82458d6e3a002025d3354f3488e609ac9
Leonardo Mota <contact@leoism.me>
2021-10-29 11:46:24 -0700

    Add Waypoint Shake & Cam

--- Assets/Scripts/WayPoint/WayPoint.cs
+++ Assets/Scripts/WayPoint/WayPoint.cs
@@ -4,41 +4,92 @@ using UnityEngine;
 
 public class WayPoint : MonoBehaviour
 {
-    private Vector3 mInitPosition = Vector3.zero;
-    private int mHitCount = 0;
-    private const int kHitLimit = 3;
-    private const float kRepositionRange = 15f; // +- this value
-    private Color mNormalColor = Color.white;
-    
-    // Start is called before the first frame update
-    void Start()
+  private WaypointCameraBehavior waypointCamera = null;
+  private Vector3 mInitPosition = Vector3.zero;
+  private int mHitCount = 0;
+  private const int kHitLimit = 3;
+  private const float kRepositionRange = 15f; // +- this value
+  private Color mNormalColor = Color.white;
+  private ShakePosition mShake = new ShakePosition(0, 5);
+  private TimedLerp mPositionLerp = new TimedLerp(0, 0);
+  private bool prevShakeStatus = false;
+  private Vector3 posBeforeShake;
+  // Start is called before the first frame update
+  void Start()
+  {
+    mInitPosition = transform.position;
+  }
+
+  void Update()
+  {
+    if (!mShake.ShakeDone())
     {
-        mInitPosition = transform.position;
+      transform.position = mShake.UpdateShake();
     }
+    if (!prevShakeStatus && mShake.ShakeDone())
+    {
+      waypointCamera.TurnOff();
+    }
+    prevShakeStatus = mShake.ShakeDone();
+  }
+
+  public void SetWaypointCamera(WaypointCameraBehavior cam)
+  {
+    waypointCamera = cam;
+  }
 
 
-    private void Reposition() 
+  private void Reposition()
+  {
+    if (mShake.ShakeDone())
     {
-        Vector3 p = mInitPosition;
-        p += new Vector3(Random.Range(-kRepositionRange, kRepositionRange),
-                         Random.Range(-kRepositionRange, kRepositionRange),
-                         0f);
-        transform.position = p;
-        GetComponent<SpriteRenderer>().color = mNormalColor;
+      Vector3 p = mInitPosition;
+      p += new Vector3(Random.Range(-kRepositionRange, kRepositionRange),
+                       Random.Range(-kRepositionRange, kRepositionRange),
+                       0f);
+      transform.position = p;
+      if (waypointCamera.IsOn())
+        waypointCamera.SetCameraPosition(new Vector3(p.x, p.y, -10));
     }
+    GetComponent<SpriteRenderer>().color = mNormalColor;
+  }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+  private void OnTriggerEnter2D(Collider2D collision)
+  {
+    if (collision.gameObject.name == "Egg(Clone)")
     {
-        if (collision.gameObject.name == "Egg(Clone)")
-        {
-            mHitCount++;
-            Color c = mNormalColor * (float)(kHitLimit - mHitCount + 1) / (float)(kHitLimit + 1);
-            GetComponent<SpriteRenderer>().color = c;
-            if (mHitCount > kHitLimit)
-            {
-                mHitCount = 0;
-                Reposition();
-            }
-        }
+      mHitCount++;
+      Color c = mNormalColor * (float)(kHitLimit - mHitCount + 1) / (float)(kHitLimit + 1);
+      GetComponent<SpriteRenderer>().color = c;
+      if (mHitCount > kHitLimit)
+      {
+        mHitCount = 0;
+        Reposition();
+      }
+      else
+        Hit();
     }
+  }
+
+  private void Hit()
+  {
+    if (!waypointCamera.IsOn())
+    {
+      waypointCamera.TurnOn();
+      waypointCamera.SetCameraPosition(transform.position);
+      posBeforeShake = transform.position;
+    }
+    SetShakeParameters(10, mHitCount);
+    Shake(new Vector2(mHitCount, mHitCount));
+  }
+
+  public void SetShakeParameters(float frequency, float durationSeconds)
+  {
+    mShake.SetShakeParameters(frequency, durationSeconds);
+  }
+
+  public void Shake(Vector2 delta)
+  {
+    mShake.SetShakeMagnitude(delta, posBeforeShake);
+  }
 }
